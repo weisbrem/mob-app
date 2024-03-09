dfa65507f065e7c23ca42ccd7bd10343017e9500
weisbrem <zhernovkov_konst@mail.ru>
2024-03-09 08:07:29 +0100

    feat: add base input component

---
+++ shared/input/input.tsx
@@ -0,0 +1,48 @@
+import { Pressable, StyleSheet, TextInput, TextInputProps, View } from 'react-native';
+import { Colors, Radius } from '../tokens';
+import EyeClosedIcon from '../../assets/icons/eye-closed';
+import EyeOpenIcon from '../../assets/icons/eye-open';
+import { useState } from 'react';
+
+interface IInputProps extends TextInputProps {
+  isPassword?: boolean;
+}
+
+export function Input({ isPassword, ...props }: IInputProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const handleChangeIsPasswordVisible = () => {
+    setIsPasswordVisible((current) => !current);
+  };
+
+  return (
+    <View>
+      <TextInput style={styles.input} secureTextEntry={isPassword && !isPasswordVisible} {...props} />
+      {isPassword && (
+        <Pressable onPress={handleChangeIsPasswordVisible} style={styles.eyeIcon}>
+          {isPasswordVisible ? <EyeOpenIcon /> : <EyeClosedIcon />}
+        </Pressable>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 58,
+    color: Colors.gray,
+    backgroundColor: Colors.violetDark,
+    paddingVertical: 20,
+    paddingHorizontal: 26,
+    borderRadius: Radius.r10,
+    fontSize: 16,
+    fontWeight: '400',
+    textAlign: 'left',
+  },
+  eyeIcon: {
+    position: 'absolute',
+    right: 0,
+    paddingHorizontal: 24,
+    paddingVertical: 18,
+  },
+});
