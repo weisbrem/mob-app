a83c62695578446615fae30a97e3000fe0468044
weisbrem <zhernovkov_konst@mail.ru>
2024-03-09 08:07:59 +0100

    feat: add base login form layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import { Input } from './shared/input/input';
+import { Colors, Gaps, Radius } from './shared/tokens';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.logo} resizeMode='contain' source={require('./assets/logo.png')} />
+
+      <View style={styles.loginForm}>
+        <Input placeholder='Email' placeholderTextColor={Colors.gray} />
+        <Input isPassword placeholder='Пароль' placeholderTextColor={Colors.gray} />
+        <Button title='Войти' />
+      </View>
+      <Text style={styles.resetLink}>Восстановить пароль</Text>
     </View>
   );
 }
@@ -13,8 +20,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingHorizontal: 55,
+    backgroundColor: Colors.black,
     justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'sans-serif',
+    rowGap: Gaps.g50,
+  },
+  logo: {
+    width: 220,
+  },
+  loginForm: {
+    rowGap: Gaps.g16,
+    alignSelf: 'stretch',
+  },
+  enterButton: {
+    color: Colors.text,
+    backgroundColor: Colors.primary,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: Radius.r10,
+    fontSize: 18,
+    lineHeight: 20,
+    fontWeight: '400',
+  },
+  resetLink: {
+    color: Colors.link,
+    fontSize: 18,
+    lineHeight: 20,
+    fontWeight: '400',
   },
 });
