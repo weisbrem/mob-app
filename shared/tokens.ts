ca2e4554dab0d2b5fc216c72fbb63645f4a23160
weisbrem <zhernovkov_konst@mail.ru>
2024-03-09 08:06:00 +0100

    feat: add base theme colors

---
+++ shared/tokens.ts
@@ -0,0 +1,18 @@
+export const Colors = {
+  black: '#16171d',
+  gray: '#afb2bf',
+  violetDark: '#2e2d3d',
+  primary: '#6c38cc',
+  primaryHover: '#452481',
+  link: '#a97bff',
+  text: '#fafafa',
+};
+
+export const Gaps = {
+  g16: 16,
+  g50: 50,
+};
+
+export const Radius = {
+  r10: 10,
+};
