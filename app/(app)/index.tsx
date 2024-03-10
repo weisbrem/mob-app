59663f73186b6205e13ab7028e92020cc278f71f
weisbrem <zhernovkov_konst@mail.ru>
2024-03-10 22:41:37 +0100

    feat: add redirect when user didn't have access token

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,7 +1,23 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { Colors } from '../../shared/tokens';
+import { useAtomValue } from 'jotai';
+import { authAtom } from '../../entities/auth/model/auth.state';
+import { useEffect } from 'react';
+import { router, useRootNavigationState } from 'expo-router';
+import { AppRoutes } from '../../shared/common.types';
 
 export default function MyCourses() {
+  const { accessToken } = useAtomValue(authAtom);
+  const state = useRootNavigationState();
+
+  useEffect(() => {
+    if (!state?.key) return;
+
+    if (!accessToken) {
+      router.replace(AppRoutes.login);
+    }
+  }, [accessToken]);
+
   return (
     <View>
       <Text style={styles.text}>Мои курсы</Text>
@@ -11,6 +27,7 @@ export default function MyCourses() {
 
 const styles = StyleSheet.create({
   text: {
-    color: Colors.text,
+    // color: Colors.text,
+    color: Colors.black,
   },
 });
