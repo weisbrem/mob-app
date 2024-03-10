29113904df9833736014d74b5bae40c43ef64689
weisbrem <zhernovkov_konst@mail.ru>
2024-03-10 22:45:54 +0100

    refactor:  move redirect into app layout

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,6 +1,15 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
+import { useAtomValue } from 'jotai';
+import { authAtom } from '../../entities/auth/model/auth.state';
+import { AppRoutes } from '../../shared/common.types';
 
 export default function AppLayout() {
+  const { accessToken } = useAtomValue(authAtom);
+
+  if (!accessToken) {
+    return <Redirect href={AppRoutes.login} />;
+  }
+
   return (
     <Stack>
       <Stack.Screen name='index' />
--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,23 +1,7 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { Colors } from '../../shared/tokens';
-import { useAtomValue } from 'jotai';
-import { authAtom } from '../../entities/auth/model/auth.state';
-import { useEffect } from 'react';
-import { router, useRootNavigationState } from 'expo-router';
-import { AppRoutes } from '../../shared/common.types';
 
 export default function MyCourses() {
-  const { accessToken } = useAtomValue(authAtom);
-  const state = useRootNavigationState();
-
-  useEffect(() => {
-    if (!state?.key) return;
-
-    if (!accessToken) {
-      router.replace(AppRoutes.login);
-    }
-  }, [accessToken]);
-
   return (
     <View>
       <Text style={styles.text}>Мои курсы</Text>
