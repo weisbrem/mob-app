199e63ffeaed2f855233c744e739b8ccd51b78a7
weisbrem <zhernovkov_konst@mail.ru>
2024-03-20 00:43:41 +0100

    refactor: add image uploader component on profile page

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,114 +1,58 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet, Alert, Image } from 'react-native';
-import {
-  launchCameraAsync,
-  launchImageLibraryAsync,
-  MediaTypeOptions,
-  useCameraPermissions,
-  useMediaLibraryPermissions,
-  PermissionStatus,
-} from 'expo-image-picker';
-import { Colors } from '../../shared/tokens';
+import { View, StyleSheet, Image } from 'react-native';
 import { Button } from '../../shared/Button/Button';
+import { Input } from '../../shared/input/input';
+import { Colors, FontFamily, FontSize, Gaps, LineHeight, Radius } from '../../shared/tokens';
+import { ImageUploader } from '../../shared/ImageUploader/ImageUploader';
 
 export default function Profile() {
   const [image, setImage] = useState<string | null>(null);
-  const [cameraPermissionInfo, requestCameraPermissionInfo] = useCameraPermissions();
-  const [libraryPermissionInfo, requestLibraryPermissionInfo] = useMediaLibraryPermissions();
-
-  const verifyCameraPermissions = async () => {
-    if (cameraPermissionInfo?.status === PermissionStatus.UNDETERMINED) {
-      const permissionResponse = await requestCameraPermissionInfo();
-      return permissionResponse.granted;
-    }
-
-    if (cameraPermissionInfo?.status === PermissionStatus.DENIED) {
-      Alert.alert('Недостаточно прав для доступа к камере');
-      return false;
-    }
-
-    return true;
-  };
-
-  const verifyMediaPermissions = async () => {
-    if (libraryPermissionInfo?.status === PermissionStatus.UNDETERMINED) {
-      const permissionResponse = await requestLibraryPermissionInfo();
-      return permissionResponse.granted;
-    }
-
-    if (libraryPermissionInfo?.status === PermissionStatus.DENIED) {
-      Alert.alert('Недостаточно прав для доступа к фото');
-      return false;
-    }
-
-    return true;
-  };
-
-  const captureAvatar = async () => {
-    const isPermissionGranted = await verifyCameraPermissions();
-
-    if (!isPermissionGranted) {
-      return;
-    }
-
-    const { assets } = await launchCameraAsync({
-      mediaTypes: MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-
-    if (!assets) {
-      return;
-    }
-
-    setImage(assets[0].uri);
-  };
-
-  const pickAvatar = async () => {
-    const isPermissionGranted = await verifyMediaPermissions();
-
-    if (!isPermissionGranted) {
-      return;
-    }
-
-    const { assets } = await launchImageLibraryAsync({
-      mediaTypes: MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-
-    if (!assets) {
-      return;
-    }
-
-    setImage(assets[0].uri);
-  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Профиль</Text>
+      <View style={styles.imageUploadContainer}>
+        {image ? (
+          <Image style={styles.image} source={{ uri: image }} />
+        ) : (
+          <Image source={require('../../assets/images/user-profile-default.png')} />
+        )}
 
-      {image && <Image source={{ uri: image, width: 70, height: 70 }} />}
-
-      <View style={styles.buttonContainer}>
-        <Button title='Сделать фото' onPress={captureAvatar} />
-        <Button title='Выбрать из галереи' onPress={pickAvatar} />
+        <ImageUploader onUpload={setImage} />
       </View>
+
+      <Input style={styles.input} placeholder='Имя' placeholderTextColor={Colors.gray} />
+      <Button title='Сохранить' onPress={() => null} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'stretch',
+    rowGap: Gaps.g24,
+    paddingTop: 32,
+    paddingHorizontal: 30,
+  },
+  imageUploadContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    paddingTop: 20,
+    justifyContent: 'center',
+    columnGap: 20,
   },
-  text: {
-    color: Colors.text,
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
   },
-  buttonContainer: {
-    rowGap: 10,
+  input: {
+    height: 58,
+    color: Colors.gray,
+    backgroundColor: Colors.violetDark,
+    paddingLeft: 26,
+    borderRadius: Radius.r10,
+    fontFamily: FontFamily.FiraSans,
+    fontSize: FontSize.f16,
+    lineHeight: LineHeight.l18,
+    fontWeight: '400',
   },
 });
