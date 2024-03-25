edc8689f7eb218aee974a0277e19e4888f1fc523
weisbrem <zhernovkov_konst@mail.ru>
2024-03-26 00:21:48 +0100

    feat: add course model

---
+++ entities/course/model/course.model.ts
@@ -0,0 +1,39 @@
+export interface IProgress {
+  progressPercent: number;
+  tariffLessonsCount: number;
+  userViewedLessonsCount: number;
+}
+
+export type TStudentCourseDescription = {
+  id: number;
+  shortTitle: string;
+  image: string;
+  title: string;
+  alias: string;
+  length: number;
+  avgRating: number;
+  price: number;
+  courseOnDirection: { direction: Record<'name', string> }[];
+  tariffs: TTariff[];
+  progress: IProgress;
+};
+
+export type TTariff = {
+  id: number;
+  name: string;
+  price: number;
+  type: TariffType;
+  oldPrice?: number;
+  creditPrice?: number;
+  lengthInMonth: number;
+  courseId: number;
+  createdAt: string;
+  videoUuid: string;
+};
+
+export enum TariffType {
+  free = 'free',
+  basic = 'basic',
+  mentor = 'mentor',
+  project = 'project',
+}
