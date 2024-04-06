e9e71651c4112d5208c834de066157565b90af54
weisbrem <zhernovkov_konst@mail.ru>
2024-04-06 21:03:44 +0200

    feat: add refresh controls

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,15 +1,17 @@
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, ActivityIndicator, RefreshControl } from 'react-native';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { courseAtom, loadCourseAtom } from '../../entities/course/model/course.state';
 import { useEffect } from 'react';
 import { CourseCard } from '../../entities/course/ui/CourseCard/CourseCard';
 import { TStudentCourseDescription } from '../../entities/course/model/course.model';
+import { Colors } from '../../shared/tokens';
 
 export default function MyCourses() {
   const loadCourse = useSetAtom(loadCourseAtom);
   const { courses, status, errorMessage } = useAtomValue(courseAtom);
 
   const hasCourse = courses.length > 0;
+  const isLoading = status === 'pending';
 
   const renderCourses = ({ item }: { item: TStudentCourseDescription }) => (
     <View style={styles.course}>
@@ -22,7 +24,24 @@ export default function MyCourses() {
   }, []);
 
   return (
-    <>{hasCourse && <FlatList data={courses} keyExtractor={({ id }) => id.toString()} renderItem={renderCourses} />}</>
+    <>
+      {isLoading && <ActivityIndicator style={styles.activityIndicator} size='large' color={Colors.primary} />}
+      {hasCourse && (
+        <FlatList
+          refreshControl={
+            <RefreshControl
+              tintColor={Colors.primary}
+              titleColor={Colors.primary}
+              refreshing={isLoading}
+              onRefresh={loadCourse}
+            />
+          }
+          data={courses}
+          keyExtractor={({ id }) => id.toString()}
+          renderItem={renderCourses}
+        />
+      )}
+    </>
   );
 }
 
@@ -30,5 +49,8 @@ const styles = StyleSheet.create({
   course: {
     paddingVertical: 9,
     paddingHorizontal: 30,
+  },
+  activityIndicator: {
+    marginTop: 30,
   },
 });
